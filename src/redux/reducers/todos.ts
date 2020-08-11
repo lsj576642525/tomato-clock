7ee7c19b2dbd3b1a75649ca37393f0272f17d2fb
linshijie <lin_shi_jie@126.com>
2020-08-11 16:46:22 +0800

    fix bug: 顺序错误

--- src/redux/reducers/todos.ts
+++ src/redux/reducers/todos.ts
@@ -3,7 +3,7 @@ import { ADD_TODO, INIT_TODOS, UPDATE_TODO, EDIT_TODO } from "../actionTypes";
 export default (state: any[] = [], action: any) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, action.payload];
+      return [action.payload, ...state];
 
     case INIT_TODOS:
       return [...action.payload];
