fca564983c5842bae1d6da7a21c7b07903f79efe
linshijie <lin_shi_jie@126.com>
2020-08-13 00:01:33 +0800

    完成折线统计图

---
+++ src/components/Statistics/Statistics.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { connect } from "react-redux";
+import Polygon from "./Polygon";
+import { format } from "date-fns";
+import _ from "lodash";
+
+interface IProps {
+  todos: any[];
+}
+
+class Statistics extends React.Component<IProps> {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  get finishedTodos() {
+    return this.props.todos.filter((t) => t.completed && !t.deleted);
+  }
+
+  get dailyTodos() {
+    const obj = _.groupBy(this.finishedTodos, (todo) => {
+      return format(Date.parse(todo.updated_at), "yyyy-MM-d");
+    });
+
+    return obj;
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>统计</li>
+          <li>目标</li>
+          <li>番茄历史</li>
+          <li>
+            任务历史 累计完成{this.finishedTodos.length}个任务
+            <Polygon
+              data={this.dailyTodos}
+              totalFinishedCount={this.finishedTodos.length}
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: any, ownProps: any) => ({
+  todos: state.todos,
+  ...ownProps,
+});
+
+export default connect(mapStateToProps)(Statistics);
