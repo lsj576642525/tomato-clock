50d5d815bbe9b904b2b37892fefc7c21d84f708a
linshijie <lin_shi_jie@126.com>
2020-08-18 19:29:14 +0800

    test gh-pages

---
+++ build/precache-manifest.01b5c63e9d25c98c78116f9a855ef3e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d798a82c63058216ed970509cdb8439",
+    "url": "./index.html"
+  },
+  {
+    "revision": "25d62f9657c489b26793",
+    "url": "./static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "f791912d727fc7d9a51e",
+    "url": "./static/css/main.e7555fec.chunk.css"
+  },
+  {
+    "revision": "25d62f9657c489b26793",
+    "url": "./static/js/2.f51f37ef.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "./static/js/2.f51f37ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f791912d727fc7d9a51e",
+    "url": "./static/js/main.861fb3eb.chunk.js"
+  },
+  {
+    "revision": "a4019095411cfffe9e04",
+    "url": "./static/js/runtime-main.4fc63226.js"
+  }
+]);
