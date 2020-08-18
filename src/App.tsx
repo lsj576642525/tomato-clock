3c931f1aa0929bc2bcfaa10f54dc289b800d9fb5
linshijie <lin_shi_jie@126.com>
2020-08-18 19:55:02 +0800

    test gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Index from "./components/Index/Index";
 import LogIn from "./components/LogIn/LogIn";
 import SignUp from "./components/SignUp/SignUp";
